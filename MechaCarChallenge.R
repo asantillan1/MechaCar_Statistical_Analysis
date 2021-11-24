f95b0d63ca12cdec63d5db785cee2b1b1d0aa56f
aadnsant1 <adan.santillan@binario-it.com>
2021-11-24 11:48:16 -0600

    File Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,15 @@ Susp_coil_results <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFac
 #create summary table with multiple columns
 total_summary <- Susp_coil_results %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
 lot_summary <- Susp_coil_results %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+
+#T-test comparing all lots to population
+t.test((Susp_coil_results$PSI),mu=1500)
+#Subset T-test per lot
+lot1 <- subset(Susp_coil_results, Susp_coil_results$Manufacturing_Lot == "Lot1")
+t.test((lot1$PSI), mu = 1500)
+
+lot2 <- subset(Susp_coil_results, Susp_coil_results$Manufacturing_Lot == "Lot2")
+t.test((lot2$PSI), mu = 1500)
+
+lot3 <- subset(Susp_coil_results, Susp_coil_results$Manufacturing_Lot == "Lot3")
+t.test((lot3$PSI), mu = 1500)
