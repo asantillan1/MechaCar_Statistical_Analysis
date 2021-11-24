c103773d56342d916b796e7ea3a73ecbbfa8435a
aadnsant1 <adan.santillan@binario-it.com>
2021-11-24 11:37:37 -0600

    File Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,13 @@
 library(dplyr)
 # Import and Read MechaCar_mpg.csv file
 mpg_results <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+#linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_results) 
+#determine p-value, r-squared
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_results)) 
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_results) #linear regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_results)) #determine p-value, r-squared
+# Import and Read Suspension Coil .csv
+Susp_coil_results <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+#create summary table with multiple columns
+total_summary <- Susp_coil_results %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+lot_summary <- Susp_coil_results %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
